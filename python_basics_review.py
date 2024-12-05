f9ee1be0c020bfec962ee7d5a111b61f4b3acf53
rjohn05-dc <rohanmathewjohn@gmail.com>
2024-12-04 18:13:03 -0700

    finished task 1 starting task 2 and finishing the rest

---
+++ python_basics_review.py
@@ -0,0 +1,13 @@
+# 1. String Manipulation
+word=input("Enter a string")
+
+print("Uppercase" + word.upper())
+print("Lowercase" + word.lower())
+print("Reversed" + word[::-1])
+print("VowelReplacement" + word.replace("a", "*").replace("e", "*").replace("i", "*").replace("o", "*").replace("u", "*"))
+
+# 2. Math Operators
+num=input("Enter the first number")
+num2=input("Enter the second number")
+
+print("Addition")
